49b80de19d093240c3d8e25b7345483690fef5b0
alpesh-dudhat <51983494+alpesh-dudhat@users.noreply.github.com>
2022-10-10 18:57:45 +0530

    1 file modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators ,ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { AuthService } from './service/auth.service';
 
@@ -13,19 +18,25 @@ export class LoginComponent implements OnInit {
   public registerForm!: FormGroup;
   returnUrl: string;
   loginForm: FormGroup;
-  constructor(    private formBuilder: FormBuilder,private router: Router,      private route: ActivatedRoute, private authservice: AuthService,) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private authservice: AuthService
+  ) {}
 
   ngOnInit() {
-
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
-  });
-  
+      password: ['', Validators.required],
+    });
+
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  get f() { return this.loginForm.controls; }
+  get f() {
+    return this.loginForm.controls;
+  }
 
   onSubmit() {
     // const username = this.registerForm.value.username;
@@ -37,25 +48,17 @@ export class LoginComponent implements OnInit {
     //     return
     // }
 
-    this.authservice.login(this.f.username.value, this.f.password.value)
-    .pipe(first())
-    .subscribe(
-        data => {
-            this.router.navigate([this.returnUrl]);
+    this.authservice
+      .login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        (data) => {
+          this.router.navigate([this.returnUrl]);
         },
-        error => {
-            // this.alertService.error(error);
-            // this.loading = false;
-        });
-
-    // this.authservice.login(username, password)
-    // .pipe(first())
-    // .subscribe(
-    //     data => {
-    //         this.router.navigate(['/user']);
-    //     },
-    //     error => {
-    //         // this.authservice.error(error);
-    //     });
+        (error) => {
+          // this.alertService.error(error);
+          // this.loading = false;
+        }
+      );
   }
 }
